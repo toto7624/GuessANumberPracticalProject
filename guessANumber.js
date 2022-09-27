afc860c71e30459baac7d181d604678659b1d20e
toto7624 <114081376+toto7624@users.noreply.github.com>
2022-09-27 15:02:25 +0300

    Add files via upload

---
+++ guessANumber.js
@@ -0,0 +1,33 @@
+function solve(){
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+
+    });
+
+    let computerGuess = Math.floor(Math.random() * 100);
+    let guess;
+
+    let recursiveAsyncReadline = function(){
+        readline.question('Guess the number (0-100): ', number => {
+            guess = Number(number)
+
+            if(guess <= 100 & guess >= 0){
+                if(guess == computerGuess){
+                    console.log('You guess it!');
+                    return readline.close();
+                }else if(guess < computerGuess){
+                    console.log('Too Low!');
+                    recursiveAsyncReadline();
+                }else if(guess > computerGuess){
+                    console.log('Too High!');
+                    recursiveAsyncReadline();
+                }
+                }else{
+                    console.log('Invalid input! Try again...');
+                    recursiveAsyncReadline();
+            }
+        });
+    }
+    recursiveAsyncReadline();
+}solve(52)
